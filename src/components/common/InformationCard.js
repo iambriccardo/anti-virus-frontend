a48603ecbd6206e9089d55b635140deb8d8a8adb
RiccardoBusetti <riccardob36@gmail.com>
2020-04-25 18:12:54 +0200

    Fix minor layout issues

--- src/components/common/InformationCard.js
+++ src/components/common/InformationCard.js
@@ -1,59 +1,35 @@
-
-import React from "react";
-import useStyles from "../Styles";
-import Typography from "@material-ui/core/Typography";
-import Card from "@material-ui/core/Card";
-import Grid from "@material-ui/core/Grid";
-import CardContent from "@material-ui/core/CardContent";
-
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import React from 'react';
+import useStyles from '../Styles';
 
 export default function InformationCard(props) {
   const styles = useStyles();
   return (
-    <Grid container direction={'row'} item xs={3}>
-        <Grid item xs={5}>
-            <Card className={styles.patientGrid}>Photo</Card>
-        </Grid>
-        <Grid item xs={7}>
-            <Card className={styles.topBarCard} variant="outlined" >
-                <CardContent alignItems="center">
-                    <Typography
-                        align="center"
-                        color="textSecondary"
-                    >
-                        {props.name}
-                    </Typography>
-                    <Typography
-                        align="center"
-                        className={styles.topBarCardValue}
-                        color="textSecondary"
-                    >
-                        {props.birthDate}
-                    </Typography>
-                    <Typography
-                        align="center"
-                        className={styles.topBarCardValue}
-                        color="textSecondary"
-                    >
-                        {props.gender}
-                    </Typography>
-                    <Typography
-                        align="center"
-                        className={styles.topBarCardValue}
-                        color="textSecondary"
-                    >
-                        {props.birthDate}
-                    </Typography>
-                    <Typography
-                        align="center"
-                        className={styles.topBarCardValue}
-                        color="textSecondary"
-                    >
-                        {props.code}
-                    </Typography>
-                </CardContent>
-            </Card>
-        </Grid>
+    <Grid container direction="row" justify="flex-start" alignItems="center" item xs={3}>
+      <Grid item>
+        <img
+          className={styles.patientGrid}
+          src="https://webservices.scientificnet.org/rest/uisdata/api/v1/people/38064/image?w=260&amp;h=260"
+        />
+      </Grid>
+      <Grid item>
+        <Typography align="center" color="textSecondary">
+          {props.name}
+        </Typography>
+        <Typography align="center" className={styles.topBarCardValue} color="textSecondary">
+          {props.birthDate}
+        </Typography>
+        <Typography align="center" className={styles.topBarCardValue} color="textSecondary">
+          {props.gender}
+        </Typography>
+        <Typography align="center" className={styles.topBarCardValue} color="textSecondary">
+          {props.birthDate}
+        </Typography>
+        <Typography align="center" className={styles.topBarCardValue} color="textSecondary">
+          {props.code}
+        </Typography>
+      </Grid>
     </Grid>
   );
 }
