7f0eaa5f77889f858c8b74083225b2f92c6f0211
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-27 10:32:05 +0000

    Deploy RiccardoBusetti/anti-virus-frontend to github.com/RiccardoBusetti/anti-virus-frontend.git:gh-pages

---
+++ precache-manifest.048b79a152f4b0ede826676a2f1983f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82cead8f4ad630d66dfc4ef4333660b1",
+    "url": "/anti-virus-frontend/index.html"
+  },
+  {
+    "revision": "9751de5f79e9b90d91be",
+    "url": "/anti-virus-frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0376f67731a54d41ca33",
+    "url": "/anti-virus-frontend/static/js/2.2012e214.chunk.js"
+  },
+  {
+    "revision": "f563bfc58f6a82c3f363b66bcbdfe56c",
+    "url": "/anti-virus-frontend/static/js/2.2012e214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9751de5f79e9b90d91be",
+    "url": "/anti-virus-frontend/static/js/main.9c7fa60f.chunk.js"
+  },
+  {
+    "revision": "d747ce93700d6b8fd44a",
+    "url": "/anti-virus-frontend/static/js/runtime-main.5575fc80.js"
+  },
+  {
+    "revision": "77e2e467207d157dfa2860a07128943f",
+    "url": "/anti-virus-frontend/static/media/logo.77e2e467.png"
+  }
+]);
